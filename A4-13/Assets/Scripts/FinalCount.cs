5c0e4730c67852e9d1d38896ca9108407751cb8b
Quentin <q.lambkin@gmail.com>
2021-11-17 20:38:09 -0500

    coin edit

---
+++ A4-13/Assets/Scripts/FinalCount.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalCount : MonoBehaviour
+{
+    /*
+    Text text;
+    public static int coinTotal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = coinTotal.ToString();
+    }*/
+}
